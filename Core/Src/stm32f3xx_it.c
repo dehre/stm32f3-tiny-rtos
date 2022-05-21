bedb7cdea58e3cb1b60a7ca67bd4c90978d41b9b
dehre <loris4526@gmail.com>
2022-05-21 16:41:47 +0200

    improve hard-fault debugging experience

--- Core/Src/stm32f3xx_it.c
+++ Core/Src/stm32f3xx_it.c
@@ -6,7 +6,64 @@
 
 #include "os_timer.h"
 
+#include "core_cm4.h"
 #include "stm32f3xx_hal.h"
+#include <stdbool.h>
+
+//==================================================================================================
+// ENUMS - STRUCTS - TYPEDEFS
+//==================================================================================================
+
+typedef struct
+{
+    uint32_t CFSReg; // Configurable Fault Status Register
+    uint16_t UFSReg; // Usage Fault Status Registers
+    uint8_t BFSReg;  // Bus Fault Status Registers
+    uint8_t MMFSReg; // Memory Manage Fault Status Registers
+    uint32_t HFSReg; // HardFault Status Register
+
+    /* Usage Faults */
+    bool DIVBYZERO;
+    bool UNALIGNED;
+    bool NOCP;
+    bool INVPC;
+    bool INVSTATE;
+    bool UNDEFINSTR;
+
+    /* Bus Faults */
+    bool BFARVALID;
+    bool LSPERR;
+    bool STKERR;
+    bool UNSTKERR;
+    bool IMPRECISERR;
+    bool PRECISERR;
+    bool IBUSERR;
+
+    /* MemManage Faults */
+    bool MMARVALID;
+    bool MLSPERR;
+    bool MSTKERR;
+    bool MUNSTKERR;
+    bool DACCVIOL;
+    bool IACCVIOL;
+
+    /* Hard Faults */
+    bool DEBUGEVT;
+    bool FORCED;
+    bool VECTTBL;
+} HardFaultStatusRegisters_t;
+
+//==================================================================================================
+// STATIC PROTOTYPES
+//==================================================================================================
+
+static void DecodeHardFault(void);
+
+//==================================================================================================
+// STATIC VARIABLES
+//==================================================================================================
+
+static __USED HardFaultStatusRegisters_t HardFaultStatusReg = {0};
 
 //==================================================================================================
 // CORTEX-M4 PROCESSOR INTERRUPTION AND EXCEPTION HANDLERS
@@ -21,12 +78,9 @@ void NMI_Handler(void)
 
 void HardFault_Handler(void)
 {
-    // TODO LORIS: use global vars to quickly check which bit is set
-    // GDB Debugging, from https://interrupt.memfault.com/blog/cortex-m-fault-debug
-    // Entire CFSR - print/x *(uint32_t *) 0xE000ED28
-    // UsageFault Status Register (UFSR) - print/x *(uint16_t *)0xE000ED2A
-    // BusFault Status Register (BFSR) - print/x *(uint8_t *)0xE000ED29
-    // MemManage Status Register (MMFSR) - print/x *(uint8_t *)0xE000ED28
+    // To determine what caused the fault, inspect the HardFaultStatusReg variable.
+    // Useful article: https://interrupt.memfault.com/blog/cortex-m-fault-debug
+    DecodeHardFault();
     while (1)
     {
     }
@@ -75,3 +129,54 @@ void SysTick_Handler(void)
 // For the available peripheral interrupt handler names,
 // please refer to the startup file (startup_stm32f3xx.s).
 //==================================================================================================
+
+//==================================================================================================
+// STATIC FUNCTIONS
+//==================================================================================================
+
+static void DecodeHardFault(void)
+{
+    uint32_t *cfsr_addr = (uint32_t *)0xE000ED28;
+    uint32_t cfsr = *cfsr_addr;
+
+    uint32_t *hfsr_addr = (uint32_t *)0xE000ED2C;
+    uint32_t hfsr = *hfsr_addr;
+
+    HardFaultStatusReg = (HardFaultStatusRegisters_t){
+        .CFSReg = cfsr,
+        .UFSReg = cfsr >> SCB_CFSR_USGFAULTSR_Pos,
+        .BFSReg = cfsr >> SCB_CFSR_BUSFAULTSR_Pos,
+        .MMFSReg = cfsr >> SCB_CFSR_MEMFAULTSR_Pos,
+        .HFSReg = hfsr,
+
+        /* Usage Faults */
+        .DIVBYZERO = cfsr & SCB_CFSR_DIVBYZERO_Msk,
+        .UNALIGNED = cfsr & SCB_CFSR_UNALIGNED_Msk,
+        .NOCP = cfsr & SCB_CFSR_NOCP_Msk,
+        .INVPC = cfsr & SCB_CFSR_INVPC_Msk,
+        .INVSTATE = cfsr & SCB_CFSR_INVSTATE_Msk,
+        .UNDEFINSTR = cfsr & SCB_CFSR_UNDEFINSTR_Msk,
+
+        /* Bus Faults */
+        .BFARVALID = cfsr & SCB_CFSR_BFARVALID_Msk,
+        .LSPERR = cfsr & SCB_CFSR_LSPERR_Msk,
+        .STKERR = cfsr & SCB_CFSR_STKERR_Msk,
+        .UNSTKERR = cfsr & SCB_CFSR_UNSTKERR_Msk,
+        .IMPRECISERR = cfsr & SCB_CFSR_IMPRECISERR_Msk,
+        .PRECISERR = cfsr & SCB_CFSR_PRECISERR_Msk,
+        .IBUSERR = cfsr & SCB_CFSR_IBUSERR_Msk,
+
+        /* MemManage Faults */
+        .MMARVALID = cfsr & SCB_CFSR_MMARVALID_Msk,
+        .MLSPERR = cfsr & SCB_CFSR_MLSPERR_Msk,
+        .MSTKERR = cfsr & SCB_CFSR_MSTKERR_Msk,
+        .MUNSTKERR = cfsr & SCB_CFSR_MUNSTKERR_Msk,
+        .DACCVIOL = cfsr & SCB_CFSR_DACCVIOL_Msk,
+        .IACCVIOL = cfsr & SCB_CFSR_IACCVIOL_Msk,
+
+        /* Hard Faults */
+        .DEBUGEVT = hfsr & SCB_HFSR_DEBUGEVT_Msk,
+        .FORCED = hfsr & SCB_HFSR_FORCED_Msk,
+        .VECTTBL = hfsr & SCB_HFSR_VECTTBL_Msk,
+    };
+}
