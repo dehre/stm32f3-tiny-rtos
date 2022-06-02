4662fd7d016f269fc8405c946bf55c0b46c56c17
dehre <loris4526@gmail.com>
2022-06-02 16:09:35 +0200

    remove unused I2C driver from HAL

--- Core/Inc/stm32f3xx_hal_conf.h
+++ Core/Inc/stm32f3xx_hal_conf.h
@@ -50,7 +50,7 @@
 #define HAL_FLASH_MODULE_ENABLED
 #define HAL_PWR_MODULE_ENABLED
 #define HAL_CORTEX_MODULE_ENABLED
-#define HAL_I2C_MODULE_ENABLED
+/* #define HAL_I2C_MODULE_ENABLED */
 
 //==================================================================================================
 // HSE/HSI VALUES ADAPTATION
